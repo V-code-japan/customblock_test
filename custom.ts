390861f26ebfa899c50230ec9ef137936fb0648e
V-code-japan <minecraft@e-sia.jp>
2025-12-20 15:37:26 +0900

    更新 main.blocks, custom.ts

--- custom.ts
+++ custom.ts
@@ -5,8 +5,26 @@
 //% weight=100 color=#ff4500 icon=""
 namespace イベントトリガー {
 
+    //% block="ブロック"
+    const my_block: {[key: string]: number} = {
+        'oak_planks': 0,
+        'birch_planks': 1,
+        'dark_oak_planks': 1
+    };
+
     //% block="イベントをトリガーする %t %e"
     export function triggerEntityEvent(t: string, e: string): void {
         player.execute(`event entity @e[type=${t}] ${e}`);
+    };
+
+    //% block="ブロックをえらぶ %n"
+    //% n.shadow="ghostBlock"
+    //% n.fieldEditor="gridpicker"
+    export function chooseBlock(n: number) {
+        if(n === 0) {
+            player.execute(`say §c正解！`);
+        } else {
+            player.execute(`say §b残念…`);
+        }
     }
 }
