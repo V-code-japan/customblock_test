c2780f6b96c0587a2de37dd4c83d621fe56958e3
V-code-japan <minecraft@e-sia.jp>
2025-12-20 13:00:56 +0900

    更新 custom.ts

--- custom.ts
+++ custom.ts
@@ -1,12 +1,12 @@
 // custom.ts
 // Custom MakeCode extension file
 
-//% block="craft with"
+//% block="trigger event"
 //% weight=100 color=#ff4500 icon=""
-namespace ブロックをおく {
+namespace イベントトリガー {
 
-    //% block="craft with %n"
-    export function craftWith(n: number): void {
-        player.execute(`scoreboard players set .output${n} global 1`);
+    //% block="イベントをトリガーする %イベント名"
+    export function triggerEntityEvent(t: string, e: string): void {
+        player.execute(`event entity @e[type=${t}] ${e}`);
     }
 }
